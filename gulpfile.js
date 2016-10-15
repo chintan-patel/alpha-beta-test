683f940d3a367f22efd9bcff0b69f125f6a59cda
PATEC036 <chintankumar.x.patel.-nd@disney.com>
2016-10-15 15:16:24 -0400

    Adding less and other injection

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var wiredep = require('wiredep').stream;
 var inject = require('gulp-inject');
+var less = require('gulp-less');
+var path = require('path');
 
 gulp.task('bower', function () {
     return gulp.src('src/index.html')
@@ -10,6 +12,24 @@ gulp.task('bower', function () {
     .pipe(gulp.dest('./dest'));
 });
 
-gulp.task('default', ['bower'],  function(){
+gulp.task('less', function () {
+  return gulp.src('./src/**/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('./dest'));
+});
+
+
+
+gulp.task('index', function () {
+  var sources = gulp.src(['./dest/**/*.js', './dest/**/*.css'], {read: false});
+
+  return gulp.src('./dest/index.html')
+  .pipe(inject(sources))
+    .pipe(gulp.dest('./dest'));
+});
+
+gulp.task('default', ['less', 'bower', 'index'],  function(){
 
 });
